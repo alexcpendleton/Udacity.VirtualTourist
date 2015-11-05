2ede18382855d32fbd21d6773d8736c7b092a692
Alex Pendleton <alexcpendleton@gmail.com>
2015-11-05 00:13:07 -0500

    Got some work done on how fetching images will work. This currently does not build but I'm too tired to keep wrestling with PromiseKit.

---
+++ VirtualTourist/VirtualTourist/ExternalImageFetchable.swift
@@ -0,0 +1,15 @@
+//
+//  ExternalImageFetchable.swift
+//  VirtualTourist
+//
+//  Created by Alex Pendleton on 11/3/15.
+//  Copyright © 2015 Alex Pendleton. All rights reserved.
+//
+
+import Foundation
+import MapKit
+import PromiseKit
+
+public protocol ExternalImageFetchable {
+    func images(forLocation:CLLocationCoordinate2D) -> Promise<[AnyObject]>
+}
